81816953ff4970b9af1952bc1c559b8226a4d321
cxh <809448054@qq.com>
2023-05-10 23:00:07 +0800

    修复定制化表select类型为空更新失败问题

--- src/main/java/com/fate1412/crmSystem/module/customTable/service/impl/CustomTableServiceImpl.java
+++ src/main/java/com/fate1412/crmSystem/module/customTable/service/impl/CustomTableServiceImpl.java
@@ -78,7 +78,7 @@ public class CustomTableServiceImpl implements ICustomTableService {
         //获取表字段
         List<TableColumnDict> tableColumnDictList = tableColumnDictService.listByTableName(tableName);
         List<Long> linkTableIds = MyCollections.objects2List(tableColumnDictList, TableColumnDict::getLinkTable);
-    
+        
         List<TableDict> linkTableList = mapper.selectBatchIds(linkTableIds);
         Map<Long, String> tableMap = MyCollections.list2MapL(linkTableList, TableDict::getId, TableDict::getTableName);
         
@@ -86,19 +86,19 @@ public class CustomTableServiceImpl implements ICustomTableService {
         JSONObject jsonObject = new JSONObject();
         
         //加入审批状态
-        TableColumnDict pass = TableColumnDict.create(tableName,"pass","","是否通过",1000).setDisabled(true).setCustom(false).setColumnType(4);
+        TableColumnDict pass = TableColumnDict.create(tableName, "pass", "", "是否通过", 1000).setDisabled(true).setCustom(false).setColumnType(4);
         
         tableColumnDictList.add(pass);
         tableColumnDictList.forEach(tableColumnDict -> {
             CustomTableColumnSelectDTO dto = new CustomTableColumnSelectDTO();
             BeanUtils.copyProperties(tableColumnDict, dto);
-    
-            jsonObject.put(dto.getColumnName(),null);
+            
+            jsonObject.put(dto.getColumnName(), null);
             //Select类型
             if (tableColumnDict.getColumnType().equals(FormType.Select.getIndex())) {
                 List<OptionDTO> optionDTOS = tableOptionService.getOptions(tableName, tableColumnDict.getColumnName());
                 dto.setOptions(optionDTOS);
-                jsonObject.put(dto.getColumnName()+"R",new IdToName(null,null,tableMap.get(dto.getLinkTable())));
+                jsonObject.put(dto.getColumnName() + "R", new IdToName(null, null, tableMap.get(dto.getLinkTable())));
             }
             dto.setLabel(dto.getShowName());
             dto.setProp(dto.getColumnName());
@@ -122,12 +122,12 @@ public class CustomTableServiceImpl implements ICustomTableService {
         String id = like.getString("id");
         String name = like.getString("name");
         if (StringUtils.isNotBlank(id)) {
-            sqlFactors.like("id",id);
+            sqlFactors.like("id", id);
         }
         if (StringUtils.isNotBlank(name)) {
-            sqlFactors.like("name",name.trim());
+            sqlFactors.like("name", name.trim());
         }
-        sqlFactors.eq("del_flag",false);
+        sqlFactors.eq("del_flag", false);
         
         CustomTableResultData tableResultData = getTableColumns(selectPage.getTableName());
         MyPage page = listByPage(selectPage.getPage().intValue(), selectPage.getPageSize().intValue(), selectPage.getTableName(), sqlFactors.getSqlFactors(), tableResultData.getTableColumns());
@@ -147,7 +147,7 @@ public class CustomTableServiceImpl implements ICustomTableService {
         TableDict tableDict = tableDictList.get(0);
         CustomTableResultData tableResultData = getTableColumns(tableName);
         SQLFactors sqlFactors = new SQLFactors();
-        sqlFactors.in("id",idList);
+        sqlFactors.in("id", idList);
         List<JSONObject> jsonObjectList = mapper.select(tableDict.getRealTableName(), sqlFactors.getSqlFactors());
         List<JSONObject> dtoList = getDTOList(tableDict.getTableName(), jsonObjectList, tableResultData.getTableColumns(), true);
         tableResultData.setTableDataList(dtoList);
@@ -175,19 +175,24 @@ public class CustomTableServiceImpl implements ICustomTableService {
         columnDictList.forEach(column -> {
             if (column.getInserted()) {
                 //设置字段名和值，用于新增
-                sqlFactors.eq(column.getRealColumnName(),getColumnData(column,jsonObject));
+                Object columnData = getColumnData(column, jsonObject);
+                if (column.getColumnType().equals(FormType.Select.getIndex()) && "".equals(columnData)) {
+                    //Select类型处理空字符串为null
+                    columnData = null;
+                }
+                sqlFactors.eq(column.getRealColumnName(), columnData);
             }
         });
         //设置必备字段数据
         Long id = IdWorker.getId();
-        sqlFactors.eq("id",id);
-        sqlFactors.eq("create_time",new Date());
-        sqlFactors.eq("update_time",new Date());
-        sqlFactors.eq("creater",thisUser.getUserId());
-        sqlFactors.eq("updater",thisUser.getUserId());
+        sqlFactors.eq("id", id);
+        sqlFactors.eq("create_time", new Date());
+        sqlFactors.eq("update_time", new Date());
+        sqlFactors.eq("creater", thisUser.getUserId());
+        sqlFactors.eq("updater", thisUser.getUserId());
         
         //插入
-        if (mapper.insertList(tableDict.getRealTableName(),MyCollections.toList(sqlFactors.getSqlFactors())) > 0) {
+        if (mapper.insertList(tableDict.getRealTableName(), MyCollections.toList(sqlFactors.getSqlFactors())) > 0) {
             flowSessionService.addFlowSession(tableDict.getTableName(), id);
             return ResultTool.success();
         } else {
@@ -215,8 +220,8 @@ public class CustomTableServiceImpl implements ICustomTableService {
         //获取当前数据
         SQLFactors dataFactors = new SQLFactors();
         dataFactors
-                .eq("id",jsonObject.getLong("id"))
-                .eq("del_flag",false);
+                .eq("id", jsonObject.getLong("id"))
+                .eq("del_flag", false);
         List<JSONObject> jsonObjectList = mapper.select(tableDict.getRealTableName(), dataFactors.getSqlFactors());
         if (MyCollections.isEmpty(jsonObjectList)) {
             throw new DataCheckingException(ResultCode.COMMON_FAIL);
@@ -226,21 +231,26 @@ public class CustomTableServiceImpl implements ICustomTableService {
         columnDictList.forEach(column -> {
             if (!column.getDisabled()) {
                 //设置字段名和值，用于编辑
-                sqlFactors.eq(column.getRealColumnName(),getColumnData(column,jsonObject));
+                Object columnData = getColumnData(column, jsonObject);
+                if (column.getColumnType().equals(FormType.Select.getIndex()) && "".equals(columnData)) {
+                    //Select类型处理空字符串为null
+                    columnData = null;
+                }
+                sqlFactors.eq(column.getRealColumnName(), columnData);
             } else {
                 if (column.getColumnName().equals("updateTime")) {
-                    sqlFactors.eq("update_time",new Date());
+                    sqlFactors.eq("update_time", new Date());
                 } else if (column.getColumnName().equals("updater")) {
-                    sqlFactors.eq("updater",thisUser.getUserId());
+                    sqlFactors.eq("updater", thisUser.getUserId());
                 } else {
                     //不更新的属性
                     sqlFactors.eq(column.getRealColumnName(), object.get(column.getRealColumnName()));
                 }
             }
         });
-    
+        
         //更新
-        if (mapper.updateList(tableDict.getRealTableName(),MyCollections.toList(sqlFactors.getSqlFactors())) > 0) {
+        if (mapper.updateList(tableDict.getRealTableName(), MyCollections.toList(sqlFactors.getSqlFactors())) > 0) {
             return ResultTool.success();
         } else {
             throw new DataCheckingException(ResultCode.UPDATE_ERROR);
@@ -254,7 +264,7 @@ public class CustomTableServiceImpl implements ICustomTableService {
             return false;
         }
         SQLFactors sqlFactors = new SQLFactors();
-        sqlFactors.eq("id",id);
+        sqlFactors.eq("id", id);
         return mapper.deleteList(tableDict.getRealTableName(), sqlFactors.getSqlFactors()) > 0;
     }
     
@@ -266,11 +276,11 @@ public class CustomTableServiceImpl implements ICustomTableService {
         }
         SQLFactors sqlFactors = new SQLFactors();
         sqlFactors
-                .like("name",nameLike.trim())
-                .eq("del_flag",false);
-        PageHelper.startPage(page,10);
+                .like("name", nameLike.trim())
+                .eq("del_flag", false);
+        PageHelper.startPage(page, 10);
         List<JSONObject> jsonObjectList = mapper.select(tableDict.getRealTableName(), sqlFactors.getSqlFactors());
-        return IdToName.createList(jsonObjectList,k -> k.getLong("id"), v -> v.getString("name"));
+        return IdToName.createList(jsonObjectList, k -> k.getLong("id"), v -> v.getString("name"));
     }
     
     private MyPage listByPage(int thisPage, int pageSize, String tableName, List<SQLFactor<Object>> sqlFactors, List<CustomTableColumnSelectDTO> columnList) {
@@ -318,7 +328,7 @@ public class CustomTableServiceImpl implements ICustomTableService {
         
         List<SysUser> sysUserList = sysUserService.listByIds(userIdList);
         Map<Long, String> userMap = MyCollections.list2MapL(sysUserList, SysUser::getUserId, SysUser::getRealName);
-    
+        
         //审批
         List<Long> dataIds = MyCollections.objects2List(jsonObjectList, (jsonObject -> jsonObject.getLong("id")));
         Map<Long, Integer> passMap = flowSessionService.getPass(tableName.trim(), dataIds);
@@ -337,9 +347,9 @@ public class CustomTableServiceImpl implements ICustomTableService {
             }
             Integer pass = passMap.get(id);
             switch (pass) {
-                case 0: object.put("pass","未审批");break;
-                case 1: object.put("pass","已通过");break;
-                default: object.put("pass","已拒绝");
+                case 0: object.put("pass", "未审批"); break;
+                case 1: object.put("pass", "已通过"); break;
+                default: object.put("pass", "已拒绝");
             }
         });
         //获取可链接的相关表
@@ -350,10 +360,10 @@ public class CustomTableServiceImpl implements ICustomTableService {
         
         columnList.forEach(column -> {
             //时间格式化
-            if(column.getColumnType().equals(FormType.DateTime.getIndex()) || column.getColumnType().equals(FormType.Date.getIndex())) {
+            if (column.getColumnType().equals(FormType.DateTime.getIndex()) || column.getColumnType().equals(FormType.Date.getIndex())) {
                 //Date类型默认使用yyyy-MM-dd hh:mm:ss格式
                 jsonObjectList.forEach(object -> {
-                    object.put(column.getColumnName(),object.getDate(column.getColumnName()));
+                    object.put(column.getColumnName(), object.getDate(column.getColumnName()));
                 });
             }
             
@@ -365,7 +375,7 @@ public class CustomTableServiceImpl implements ICustomTableService {
                 sqlFactors.in("id", ids);
                 List<JSONObject> select = mapper.select(realTableMap.get(column.getLinkTable()), sqlFactors.getSqlFactors());
                 Map<Long, String> map = MyCollections.list2MapL(select, (k) -> k.getLong("id"),
-                        (v) -> v.getString("name") == null? v.getString("id") : v.getString("name"));
+                        (v) -> v.getString("name") == null ? v.getString("id") : v.getString("name"));
                 jsonObjectList.forEach(object -> {
                     String columnName = column.getColumnName();
                     String columnNameR = column.getColumnName() + "R";
